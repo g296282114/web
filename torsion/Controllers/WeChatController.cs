b07256e3ec848e3afa6e7eddfb909842bf3fe94e
g296282114 <296282114@qq.com>
2015-03-30 22:37:46 +0800

    notebook

---
+++ torsion/Controllers/WeChatController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace torsion.Controllers
+{
+    public class WeChatController : Controller
+    {
+        //
+        // GET: /WeChat/
+
+        private static readonly string Token = "weixin";
+        //
+        // GET: /Weixin/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            string echoStr = Request.QueryString["echoStr"];
+            if (CheckSignature())
+            {
+                if (!string.IsNullOrEmpty(echoStr))
+                {
+                    return Content(echoStr);
+                }
+            }
+            return null;
+        }
+        /// <summary>
+        /// 验证微信签名
+        /// </summary>
+        /// * 将token、timestamp、nonce三个参数进行字典序排序
+        /// * 将三个参数字符串拼接成一个字符串进行sha1加密
+        /// * 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信。
+        /// <returns></returns>
+        private bool CheckSignature()
+        {
+            string signature = Request.QueryString["signature"];
+            string timestamp = Request.QueryString["timestamp"];
+            string nonce = Request.QueryString["nonce"];
+            string[] ArrTmp = { Token, timestamp, nonce };
+            Array.Sort(ArrTmp);     //字典排序
+            string tmpStr = string.Join("", ArrTmp);
+            tmpStr = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(tmpStr, "SHA1");
+            tmpStr = tmpStr.ToLower();
+            if (tmpStr == signature)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void Valid()
+        {
+            string echoStr = Request.QueryString["echoStr"];
+            if (CheckSignature())
+            {
+                if (!string.IsNullOrEmpty(echoStr))
+                {
+                    Response.Write(echoStr);
+                    Response.End();
+                }
+            }
+        }
+
+    }
+}
