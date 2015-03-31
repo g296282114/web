8002956cdf7b53ff4dba82083e9a4f85cacec6a3
g296282114 <296282114@qq.com>
2015-03-31 20:42:00 +0800

    notebook

--- torsion/Controllers/WeChatController.cs
+++ torsion/Controllers/WeChatController.cs
@@ -243,7 +243,8 @@ namespace torsion.Controllers
         }
 
         public ActionResult creat_mymenu() 
-        { 
+        {
+            finalstr = "Tm8WCsRvHCrsrJpejES-BDlZqDLSiXvRyYjQ_TyifZ_61LxR7eSxf57gwvSTp8ws8bYsEnMqHwoErcRZFIeMR0HLO8-1lw5bm6Bwx0Bx9Ig";
             string url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=" + 
                 finalstr; 
             string data = "{\"button\":[{\"type\":\"click\",\"name\":\"今日歌曲\",\"key\":\"V1001_TODAY_MUSIC\"},{\"type\":\"click\",\"name\":\"歌手简介\",\"key\":\"V1001_TODAY_SINGER\"},{\"name\":\"菜单\",\"sub_button\":[{\"type\":\"view\",\"name\":\"搜索\",\"url\":\"http://www.soso.com/\"},{\"type\":\"view\",\"name\":\"视频\",\"url\":\"http://v.qq.com/\"},{\"type\":\"click\",\"name\":\"赞一下我们\",\"key\":\"V1001_GOOD\"}]}]}";
