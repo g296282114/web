cabbd595f3d07a5d8ad137a4e35ad313345bf8d6
g296282114 <296282114@qq.com>
2015-03-31 20:03:12 +0800

    notebook

--- torsion/Controllers/WeChatController.cs
+++ torsion/Controllers/WeChatController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.IO;
 using System.Text;
+using System.Xml;
+using torsion.Models;
 
 
 namespace torsion.Controllers
@@ -53,7 +55,7 @@ namespace torsion.Controllers
 
         [HttpPost]
         [ActionName("Index")]
-        public ActionResult PostIndex()
+        public void PostIndex()
         {
             Stream s = System.Web.HttpContext.Current.Request.InputStream;
             //转换成Byte数组
@@ -64,7 +66,7 @@ namespace torsion.Controllers
             string postStr = Encoding.UTF8.GetString(b);
 
             WriteFile(Server.MapPath("~/log.txt"), "Post:" + postStr);
-            return null;
+            Handle(postStr);
             
         }
         /// <summary>
@@ -104,6 +106,108 @@ namespace torsion.Controllers
                     Response.End();
                 }
             }
+        }
+
+        public void Handle(string postStr)
+        {
+            //封装请求类  
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(postStr);
+            XmlElement rootElement = doc.DocumentElement;
+            //MsgType  
+            XmlNode MsgType = rootElement.SelectSingleNode("MsgType");
+            //接收的值--->接收消息类(也称为消息推送)  
+            WC_RequestXml requestXML = new WC_RequestXml();
+            requestXML.ToUserName = rootElement.SelectSingleNode("ToUserName").InnerText;
+            requestXML.FromUserName = rootElement.SelectSingleNode("FromUserName").InnerText;
+            requestXML.CreateTime = rootElement.SelectSingleNode("CreateTime").InnerText;
+            requestXML.MsgType = MsgType.InnerText;
+
+            //根据不同的类型进行不同的处理  
+            switch (requestXML.MsgType)
+            {
+                case "text": //文本消息  
+                    requestXML.Content = rootElement.SelectSingleNode("Content").InnerText;
+                    break;
+                case "image": //图片  
+                    requestXML.PicUrl = rootElement.SelectSingleNode("PicUrl").InnerText;
+                    break;
+                case "location": //位置  
+                    requestXML.Location_X = rootElement.SelectSingleNode("Location_X").InnerText;
+                    requestXML.Location_Y = rootElement.SelectSingleNode("Location_Y").InnerText;
+                    requestXML.Scale = rootElement.SelectSingleNode("Scale").InnerText;
+                    requestXML.Label = rootElement.SelectSingleNode("Label").InnerText;
+                    break;
+                case "link": //链接  
+                    break;
+                case "event": //事件推送 支持V4.5+  
+                    break;
+            }
+
+            //消息回复  
+            ResponseMsg(requestXML);
+        }
+
+        private void ResponseMsg(WC_RequestXml requestXML)
+        {
+            try
+            {
+                string resxml = "";
+                //主要是调用数据库进行关键词匹配自动回复内容，可以根据自己的业务情况编写。  
+                //1.通常有，没有匹配任何指令时，返回帮助信息  
+                // AutoResponse mi = new AutoResponse(requestXML.Content, requestXML.FromUserName);
+
+                switch (requestXML.MsgType)
+                {
+                    case "text":
+                        //在这里执行一系列操作，从而实现自动回复内容.   
+                        // WriteLog(requestXML.Content);
+                        resxml = "<xml><ToUserName><![CDATA[" + requestXML.FromUserName + "]]></ToUserName><FromUserName><![CDATA[" + requestXML.ToUserName + "]]></FromUserName><CreateTime>" + ConvertDateTimeInt(DateTime.Now) + "</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[" + requestXML.Content + "]]></Content></xml>";
+                        // resxml += mi.GetRePic(requestXML.FromUserName);
+
+                        break;
+                    case "location":
+
+                        break;
+
+                    case "image":
+                        //图文混合的消息 具体格式请见官方API“回复图文消息”   
+                        break;
+                }
+
+                System.Web.HttpContext.Current.Response.Write(resxml);
+                //  WriteToDB(requestXML, resxml, mi.pid);
+            }
+            catch (Exception E)
+            {
+                //WriteTxt("异常：" + ex.Message + "Struck:" + ex.StackTrace.ToString());  
+                //wx_logs.MyInsert("异常：" + ex.Message + "Struck:" + ex.StackTrace.ToString());  
+            }
+        }
+
+        /// <summary>
+        /// 将Unix时间戳转换为DateTime类型时间
+        /// </summary>
+        /// <param name="d">double 型数字</param>
+        /// <returns>DateTime</returns>
+        public static System.DateTime ConvertIntDateTime(double d)
+        {
+            System.DateTime time = System.DateTime.MinValue;
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            time = startTime.AddSeconds(d);
+            return time;
+        }
+        /// <summary>
+        /// 将c# DateTime时间格式转换为Unix时间戳格式
+        /// </summary>
+        /// <param name="time">时间</param>
+        /// <returns>double</returns>
+        public static double ConvertDateTimeInt(System.DateTime time)
+        {
+            double intResult = 0;
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            intResult = (time - startTime).TotalSeconds;
+            return intResult;
         }
 
     }
