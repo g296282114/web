bdd6a6cb06e94b425b8ef817e0a0ea388f1920b2
g296282114 <296282114@qq.com>
2015-07-24 11:20:10 +0800

    send

--- torsion/Controllers/BSController.cs
+++ torsion/Controllers/BSController.cs
@@ -13,7 +13,45 @@ namespace torsion.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            int ti = 0;
+            string[] tsr = new string[] { "home", "home1", "attendance/test", "attendance/test", "home/test", "home/test", "home/test", "home1", "attendance/test", "attendance/test", "home/test", "home/test", "home/test", "home1", "attendance/test", "attendance/test", "home/test", "home/test", "home/test", "home1", "attendance/test", "attendance/test", "home/test", "home/test", "home/test", "home1", "attendance/test", "attendance/test", "home/test", "home/test", "home/test" };
+            int mn = 4;
+            Models.Menus mm = new Models.Menus();
+            mm.hm = new Models.Menus.HomeMenu[mn];
+            for (int i = 0; i < mn; i++)
+            {
+                mm.hm[i] = new Models.Menus.HomeMenu();
+                mm.hm[i].id = i;
+                mm.hm[i].name = "Menu" + i;
+                mm.hm[i].url = tsr[ti++];
+                switch(i)
+                {
+                    case 1:
+                        mm.hm[i].ico = "icon-user";
+                        break;
+                    case 2:
+                        mm.hm[i].ico = "icon-download-alt";
+                        break;
+                    case 3:
+                        mm.hm[i].ico = "icon-signal";
+                        break;
+                    default:
+                        mm.hm[i].ico = "icon-camera";
+                        break;
+                }
+                mm.hm[i].shm = new Models.Menus.SubHomeMenu[i];
+                for (int j = 0; j < i; j++)
+                {
+                    mm.hm[i].shm[j] = new Models.Menus.SubHomeMenu();
+                    mm.hm[i].shm[j].id = i*100+j;
+                    mm.hm[i].shm[j].name = "Sub" + mm.hm[i].shm[j].id;
+                    mm.hm[i].shm[j].url = tsr[ti++];
+
+                }
+            }
+
+
+            return View(mm);
         }
         public ActionResult Test()
         {
---
+++ torsion/Models/Menus.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace torsion.Models
+{
+    public class Menus
+    {
+        //
+        // GET: /Menus/
+        public HomeMenu[] hm;
+        public class HomeMenu
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string ico { get; set; }
+            public string url { get; set; }
+            public SubHomeMenu[] shm;
+        }
+
+        public class SubHomeMenu
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string url { get; set; }
+        }
+
+    }
+}
